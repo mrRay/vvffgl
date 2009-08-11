8a54489ce64c9272086d775989c0d1b3cd2b48ee
vade <vade@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2009-08-11 21:22:01 +0000

    Some basic GL stuff, and resource initialization that is required.

--- vvopensource/VVFFGL/FrameworkSrc/FFGLGPURenderer.h
+++ vvopensource/VVFFGL/FrameworkSrc/FFGLGPURenderer.h
@@ -8,11 +8,17 @@
 
 #import <Cocoa/Cocoa.h>
 #import "FFGLRenderer.h"
+#import <OpenGL/OpenGL.h>
 
 typedef struct FFGLGPURendererData FFGLGPURendererData;
 
 @interface FFGLGPURenderer : FFGLRenderer {
     FFGLGPURendererData *_data;
+	
+	CGLContextObj _context;		// prob a good idea to cache the context.
+	GLuint _rectToSquareFBO;	// this FBO is responsible for providing the GL_TEXTURE_2D texture that FFGL requires.
+	GLuint _squareFBOTexture;	// COLOR_ATTACHMENT_0 for our above FBO
+	GLint _previousFBO;			// our previously bound FBO so when we pop out of one we dont mess up the stack.
 }
 
 @end
--- vvopensource/VVFFGL/FrameworkSrc/FFGLGPURenderer.m
+++ vvopensource/VVFFGL/FrameworkSrc/FFGLGPURenderer.m
@@ -44,6 +44,43 @@ struct FFGLGPURendererData {
             [self release];
             return nil;
         }
+		
+		// retain GL context
+		_context = cgl_ctx;
+		CGLRetainContext(_context);
+		
+		// make the rect to 2D texture FBO.
+		CGLLockContext(cgl_ctx);
+		
+		glGenTextures(1, &_squareFBOTexture);
+		glBindTexture(GL_TEXTURE_2D, _squareFBOTexture);
+		
+		// NOTE: we need to get the size in from the input image. So, we may end making temp textures rather than keeping one around.
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, bounds.size.width, bounds.size.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		
+		// cache previous FBO.
+		glGetIntegerv(GL_FRAMEBUFFER_BINDING_EXT, &_previousFBO);
+		
+		// Create temporary FBO to render in texture 
+		glGenFramebuffersEXT(1, &_rectToSquareFBO);
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, _rectToSquareFBO);
+		glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, _squareFBOTexture, 0);
+		
+		GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+		if(status != GL_FRAMEBUFFER_COMPLETE_EXT)
+		{	
+			glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, _previousFBO);
+			glDeleteFramebuffersEXT(1, &_rectToSquareFBO);
+			glDeleteTextures(1, &_squareFBOTexture);
+			
+			CGLUnlockContext(cgl_ctx);
+			NSLog(@"Cannot create FFGL FBO");
+			return nil;
+		}	
+		
+		// looks like everything worked... cleanup!
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, _previousFBO);
+		CGLUnlockContext(cgl_ctx);
     }
     return self;
 }
