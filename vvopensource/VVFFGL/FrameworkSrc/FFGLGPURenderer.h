861d73af1e707dfe2f980eb24f28e743357b7667
bangnoise <bangnoise@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2009-10-05 13:22:56 +0000

    Begin to integrate FFGLImage into the rest of the framework.

--- vvopensource/VVFFGL/FrameworkSrc/FFGLGPURenderer.h
+++ vvopensource/VVFFGL/FrameworkSrc/FFGLGPURenderer.h
@@ -10,14 +10,14 @@
 #import "FFGLPluginInstances.h"
 #import <OpenGL/OpenGL.h>
 
-typedef struct FFGLGPURendererData FFGLGPURendererData;
+//typedef struct FFGLGPURendererData FFGLGPURendererData;
 
 @interface FFGLGPURenderer : FFGLRenderer {
 @private
     FFGLProcessGLStruct _frameStruct;
     CGLContextObj _context;		// prob a good idea to cache the context. - superclass has it, should expose it, so [self context] would return it... but cache it if you like :)
-    GLuint _rectToSquareFBO;	// this FBO is responsible for providing the GL_TEXTURE_2D texture that FFGL requires.
-    GLuint _squareFBOTexture;	// COLOR_ATTACHMENT_0 for our above FBO
-    GLint _previousFBO;			// our previously bound FBO so when we pop out of one we dont mess up the stack.
+//    GLuint _rectToSquareFBO;	// this FBO is responsible for providing the GL_TEXTURE_2D texture that FFGL requires.
+//    GLuint _squareFBOTexture;	// COLOR_ATTACHMENT_0 for our above FBO
+//    GLint _previousFBO;			// our previously bound FBO so when we pop out of one we dont mess up the stack.
 }
 @end
