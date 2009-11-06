c53f655fae1316fdfb71c890194327e1bc42984d
bangnoise <bangnoise@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2009-11-06 02:20:31 +0000

    Optimisation in pools.

--- VVFFGL/FrameworkSource/FFGLPool.c
+++ VVFFGL/FrameworkSource/FFGLPool.c
@@ -35,6 +35,7 @@ typedef struct FFGLPool
     FFGLPoolCallBacks callbacks;
     const void *useri;
     unsigned int size;
+    unsigned int level;
     FFGLPoolObjectRef *members;
 } FFGLPool;
 
@@ -48,25 +49,23 @@ typedef struct FFGLPoolObject
 FFGLPoolRef FFGLPoolCreate(FFGLPoolCallBacks *callbacks, unsigned int sizeMax, const void *userInfo)
 {
     if (callbacks == NULL || sizeMax == 0)
-	return NULL;
+        return NULL;
     FFGLPoolRef new = malloc(sizeof(FFGLPool));
     if (new)
     {
-	new->members = malloc(sizeof(FFGLPoolObjectRef) * sizeMax);
-	if (new->members == NULL)
-	{
-	    free(new);
-	    return NULL;
-	}
-	for (unsigned int i = 0; i < sizeMax; i++) {
-	    new->members[i] = NULL;
-	}
-	new->callbacks.create = callbacks->create;
-	new->callbacks.destroy = callbacks->destroy;
-	new->useri = userInfo;
-	new->size = sizeMax;
-	new->lock = OS_SPINLOCK_INIT;
-	new->retainc = 1;
+        new->members = malloc(sizeof(FFGLPoolObjectRef) * sizeMax);
+        if (new->members == NULL)
+        {
+            free(new);
+            return NULL;
+        }
+        new->callbacks.create = callbacks->create;
+        new->callbacks.destroy = callbacks->destroy;
+        new->useri = userInfo;
+        new->size = sizeMax;
+        new->level = 0;
+        new->lock = OS_SPINLOCK_INIT;
+        new->retainc = 1;
     }
     return new;
 }
@@ -74,7 +73,7 @@ FFGLPoolRef FFGLPoolCreate(FFGLPoolCallBacks *callbacks, unsigned int sizeMax, c
 FFGLPoolRef FFGLPoolRetain(FFGLPoolRef pool)
 {
     if (pool)
-	OSAtomicIncrement32Barrier(&pool->retainc);
+        OSAtomicIncrement32Barrier(&pool->retainc);
     return pool;
 }
 
@@ -82,65 +81,58 @@ void FFGLPoolRelease(FFGLPoolRef pool)
 {
     if (pool)
     {
-	if (OSAtomicDecrement32Barrier(&pool->retainc) == 0) {
-	    for (unsigned int i = 0; i < pool->size; i++) {
-		FFGLPoolObjectRef object = pool->members[i];
-		if (object)
-		    _FFGLPoolObjectDispose(pool->members[i]);
-	    }
-	    free(pool->members);
-	    free(pool);
-	}
+        if (OSAtomicDecrement32Barrier(&pool->retainc) == 0) {
+            for (unsigned int i = 0; i < pool->level; i++)
+            {
+                _FFGLPoolObjectDispose(pool->members[i]);
+            }
+            free(pool->members);
+            free(pool);
+        }
     }
 }
 
 FFGLPoolObjectRef FFGLPoolObjectCreate(FFGLPoolRef pool)
 {
     if (pool == NULL)
-	return NULL;
+        return NULL;
     FFGLPoolObjectRef new = NULL;
-    unsigned int i = 0;
     // lock pool for mutation
     OSSpinLockLock(&pool->lock);
-    // search pool for available object
-    while ((i < pool->size) && (new = pool->members[i]) == NULL) {
-	i++;
+    // check pool for available object
+    if (pool->level > 0)
+    {
+        // the pool has objects, so decrement the level counter
+        // and get the top-most one
+        new = pool->members[--pool->level];
     }
+    // unlock, mutation over
+    OSSpinLockUnlock(&pool->lock);
     if (new == NULL)
     {
-	// no object was found
-	// we won't mutate the pool, so unlock now
-	OSSpinLockUnlock(&pool->lock);
-	// create a new object
-	new = malloc(sizeof(FFGLPoolObject));
-	if (new)
-	{
-	    new->object = pool->callbacks.create(pool->useri);
-	    new->pool = pool;
-	    if (new->object == NULL)
-	    {
-		// empty objects are no use to man nor beast
-		// consider this failure
-		free(new);
-		new = NULL;
-	    }
-	}
-    }
-    else
-    {
-	// we found an existing object we can reuse
-	// so remove it from the pool
-	pool->members[i] = NULL;
-	// then unlock, mutation over
-	OSSpinLockUnlock(&pool->lock);
+        // no object was available
+        // create a new object
+        new = malloc(sizeof(FFGLPoolObject));
+        if (new)
+        {
+            new->object = pool->callbacks.create(pool->useri);
+            new->pool = pool;
+            if (new->object == NULL)
+            {
+                // empty objects are no use to man nor beast
+                // consider this failure
+                free(new);
+                new = NULL;
+            }
+        }
     }
     // At this point we have the only reference to the object
     if (new != NULL)
     {
-	new->retainc = 1;
-	// objects retain their pool so they can return to it
-	// and/or use its destroy callback.
-	FFGLPoolRetain(pool);
+        new->retainc = 1;
+        // objects retain their pool so they can return to it
+        // and/or use its destroy callback.
+        FFGLPoolRetain(pool);
     }
     return new;
 }
@@ -148,7 +140,7 @@ FFGLPoolObjectRef FFGLPoolObjectCreate(FFGLPoolRef pool)
 FFGLPoolObjectRef FFGLPoolObjectRetain(FFGLPoolObjectRef object)
 {
     if (object)
-	OSAtomicIncrement32Barrier(&object->retainc);
+        OSAtomicIncrement32Barrier(&object->retainc);
     return object;
 }
 
@@ -156,38 +148,34 @@ void FFGLPoolObjectRelease(FFGLPoolObjectRef object)
 {
     if (object)
     {
-	if (OSAtomicDecrement32Barrier(&object->retainc) == 0)
-	{
-	    FFGLPoolRef pool = object->pool;
-	    if (pool != NULL)
-	    {
-		unsigned int i = 0;
-		// lock pool for mutation
-		OSSpinLockLock(&pool->lock);
-		// check to see if the pool has space for us to return to it
-		while ((i < pool->size) && (pool->members[i]) != NULL) {
-		    i++;
-		}
-		if (i < pool->size && pool->members[i] == NULL)
-		{
-		    // we found an empty slot
-		    // get into it
-		    pool->members[i] = object;
-		    // unlock, mutation over
-		    OSSpinLockUnlock(&pool->lock);
-		}
-		else
-		{
-		    // the pool didn't have room, so it's time to die
-		    // we're not going to mutate the pool, so unlock
-		    OSSpinLockUnlock(&pool->lock);
-		    _FFGLPoolObjectDispose(object);
-		}
-		// objects don't retain the pool while they are in it,
-		// otherwise it would never be released.
-		FFGLPoolRelease(pool);
-	    }
-	}
+        if (OSAtomicDecrement32Barrier(&object->retainc) == 0)
+        {
+            FFGLPoolRef pool = object->pool;
+            if (pool != NULL)
+            {
+                // lock pool for mutation
+                OSSpinLockLock(&pool->lock);
+                // check to see if the pool has space for us to return to it
+                if (pool->level < pool->size)
+                {
+                    // get onto the top of the pool
+                    // and incremement the level counter
+                    pool->members[pool->level++] = object;
+                    // unlock, mutation over
+                    OSSpinLockUnlock(&pool->lock);
+                }
+                else
+                {
+                    // the pool didn't have room, so it's time to die
+                    // we're not going to mutate the pool, so unlock
+                    OSSpinLockUnlock(&pool->lock);
+                    _FFGLPoolObjectDispose(object);
+                }
+                // objects don't retain the pool while they are in it,
+                // otherwise it would never be released.
+                FFGLPoolRelease(pool);
+            }
+        }
     }
 }
 
@@ -196,7 +184,7 @@ static void _FFGLPoolObjectDispose(FFGLPoolObjectRef object)
     // this is called either from pool or object release
     // in either case, the pool is retained until this has returned
     if (object->pool->callbacks.destroy != NULL)
-	object->pool->callbacks.destroy(object->object, object->pool->useri);
+        object->pool->callbacks.destroy(object->object, object->pool->useri);
     free(object);
 }
 
