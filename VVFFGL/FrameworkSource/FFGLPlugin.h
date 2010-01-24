09248a86fd8273ef570b4e5367aebb63265d0253
bangnoise <bangnoise@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2010-01-24 15:14:43 +0000

    Documentation in headers.

--- VVFFGL/FrameworkSource/FFGLPlugin.h
+++ VVFFGL/FrameworkSource/FFGLPlugin.h
@@ -7,11 +7,15 @@
 
 #import <Cocoa/Cocoa.h>
 
+#pragma mark Constants
+
 typedef NSUInteger FFGLPluginType;
 enum {
     FFGLPluginTypeEffect = 0,
     FFGLPluginTypeSource = 1
 };
+
+#pragma mark Pixel-Formats
 
 extern NSString * const FFGLPixelFormatARGB8888;
 extern NSString * const FFGLPixelFormatBGRA8888;
@@ -26,6 +30,7 @@ enum {
     FFGLPluginModeGPU = 1
 };
 
+#pragma mark Plugin Attributes
 extern NSString * const FFGLPluginAttributeNameKey;
 extern NSString * const FFGLPluginAttributeVersionKey;
 extern NSString * const FFGLPluginAttributeDescriptionKey;
@@ -33,30 +38,145 @@ extern NSString * const FFGLPluginAttributeIdentifierKey;
 extern NSString * const FFGLPluginAttributeAuthorKey;
 extern NSString * const FFGLPluginAttributePathKey;
 
-extern NSString * const FFGLParameterAttributeTypeKey;
-extern NSString * const FFGLParameterAttributeNameKey;
-extern NSString * const FFGLParameterAttributeDefaultValueKey; // For Boolean, Number & String types.
-extern NSString * const FFGLParameterAttributeMinimumValueKey; // For Number types.
-extern NSString * const FFGLParameterAttributeMaximumValueKey; // For Number types.
-extern NSString * const FFGLParameterAttributeRequiredKey; // A NSNumber with a BOOL value.
 
-extern NSString * const FFGLParameterTypeBoolean; // A NSNumber with a BOOL value.
-extern NSString * const FFGLParameterTypeEvent; // A NSNumber with a BOOL value.
-extern NSString * const FFGLParameterTypeNumber; // A NSNumber between 0.0 and 1.0.
-extern NSString * const FFGLParameterTypeString; // A NSString.
-extern NSString * const FFGLParameterTypeImage; // A FFGLImage.
+#pragma mark Parameter Attributes
+/*
+ FFGLParameterAttributeTypeKey
+
+	The object returned for this key is one of the following NSStrings:
+
+		FFGLParameterTypeBoolean;
+		FFGLParameterTypeEvent;
+		FFGLParameterTypeNumber;
+		FFGLParameterTypeString;
+		FFGLParameterTypeImage;
+ */
+extern NSString * const FFGLParameterAttributeTypeKey;
+
+/*
+ FFGLParameterAttributeNameKey
+ 
+	The object returned for this key is a NSString with the name of the parameter. This string should be used
+	to describe the parameter in the user interface.
+ */
+extern NSString * const FFGLParameterAttributeNameKey;
+
+/*
+ FFGLParameterAttributeDefaultValueKey
+ 
+	For boolean parameters, the object returned for this key is a NSNumber with a boolean value.
+	For number paramaters, the object returned for this key is a NSNumber with a float value between 0.0 and 1.0.
+	For string paramaters, the object returned for this key is a NSString.
+ */
+extern NSString * const FFGLParameterAttributeDefaultValueKey;
+
+/*
+ FFGLParameterAttributeMinimumValueKey
+ 
+	For number parameters, the object returned for this key is a NSNumber with float value 0.0.
+ */
+extern NSString * const FFGLParameterAttributeMinimumValueKey;
+
+/*
+ FFGLParameterAttributeMaximumValueKey
+ 
+	For number parameters, the object returned for this key is a NSNumber with float value 1.0.
+ */
+extern NSString * const FFGLParameterAttributeMaximumValueKey;
+
+/*
+ FFGLParameterAttributeRequiredKey
+ 
+	For any parameter, the object returned for this key is a NSNumber with a bool value indicating
+	if that parameter must be set for rendering to succeed. This value will always be YES for
+	boolean, event, number or string inputs. Some image inputs may be optional.
+ */
+extern NSString * const FFGLParameterAttributeRequiredKey;
+
+#pragma mark Parameter Types
+extern NSString * const FFGLParameterTypeBoolean;
+extern NSString * const FFGLParameterTypeEvent;
+extern NSString * const FFGLParameterTypeNumber;
+extern NSString * const FFGLParameterTypeString;
+extern NSString * const FFGLParameterTypeImage;
 
 typedef struct FFGLPluginData FFGLPluginData; // Private
 
-@interface FFGLPlugin : NSObject <NSCopying>{
+@interface FFGLPlugin : NSObject <NSCopying> {
 @private
     FFGLPluginData *_pluginData;
 }
+
+/*
+ - (id)initWithPath:(NSString *)path
+
+	Returns a FFGLPlugin for the FreeFrame plugin at path, if one exists.
+	path should be the path to a FreeFrame plugin.
+ */
+ 
 - (id)initWithPath:(NSString *)path;
+
+/*
+ - (FFGLPluginType)type
+	
+	Returns one of
+		FFGLPluginTypeEffect
+		FFGLPluginTypeSource
+	An effect plugin typically has one or more image parameter.
+	A source plugin typically has no image parameters.
+*/
 - (FFGLPluginType)type;
+
+/*
+ - (FFGLPluginMode)mode
+ 
+	Returns one of
+		FFGLPluginModeCPU
+		FFGLPluginModeGPU
+	CPU-mode plugins run in main memory, GPU-mode plugins run on the graphics card.
+ */
 - (FFGLPluginMode)mode;
-- (NSArray *)supportedBufferPixelFormats; // An NSArray of NSStrings. 
+
+/*
+ - (NSArray *)supportedBufferPixelFormats
+ 
+	Returns an array of NSStrings indicating the pixel-formats supported. These may be some of
+		FFGLPixelFormatARGB8888
+		FFGLPixelFormatBGRA8888
+		FFGLPixelFormatRGB888
+		FFGLPixelFormatBGR888
+		FFGLPixelFormatRGB565
+		FFGLPixelFormatBGR565
+ */
+- (NSArray *)supportedBufferPixelFormats;
+
+/*
+ - (NSDictionary *)attributes
+ 
+	Returns a dictionary with information about the plugin. The dictionary may have some or all
+	of the following keys:
+		FFGLPluginAttributeNameKey
+		FFGLPluginAttributeVersionKey
+		FFGLPluginAttributeDescriptionKey
+		FFGLPluginAttributeIdentifierKey
+		FFGLPluginAttributeAuthorKey
+		FFGLPluginAttributePathKey
+ */
 - (NSDictionary *)attributes;
+
+/*
+ - (NSArray *)parameterKeys
+	Returns an array of NSStrings representing the plugin's parameters.
+ */
 - (NSArray *)parameterKeys;
+
+/*
+ - (NSDictionary *)attributesForParameterWithKey:(NSString *)key
+	
+	Returns a dictionary with information about the parameter specified by key.
+	See the FFGLParameterAttribute... constants, above, for a description of the
+	contents of this dictionary.
+	key should be one of the keys returned by parameterKeys.
+ */
 - (NSDictionary *)attributesForParameterWithKey:(NSString *)key;
 @end
--- VVFFGL/FrameworkSource/FFGLRenderer.h
+++ VVFFGL/FrameworkSource/FFGLRenderer.h
@@ -8,7 +8,7 @@
 #import <Cocoa/Cocoa.h>
 #import <OpenGL/OpenGL.h>
 #import <libkern/OSAtomic.h>
-#import <pthread.h> // maybe obscure our ivars in a struct and move this to the .m?
+#import <pthread.h> // TODO: maybe obscure our ivars in a struct and move this to the .m?
 
 @class FFGLPlugin, FFGLImage;
 
@@ -22,8 +22,6 @@ typedef NSUInteger FFGLRendererHint;
 
 @interface FFGLRenderer : NSObject
 {
-// TOM, you will probably hate this, but just for me dicking around...
-    // ANTON, ha, let's roll with it, saves duplicating it all in subclasses.
 @protected
     FFGLRendererHint	_outputHint;
     FFGLPlugin          *_plugin;
@@ -44,29 +42,62 @@ typedef NSUInteger FFGLRendererHint;
  - (id)initWithPlugin:(FFGLPlugin *)plugin context:(CGLContextObj)context pixelFormat:(NSString *)format outputHint:(FFGLRendererHint)hint size:(NSSize)size
     Initializes a new renderer.
     pixelFormat must be one of the pixel-formats supported by plugin. If plugin is a GPU plugin, pixelFormat may be nil.
-    hint is provided to the renderer to indicate your intentions for the output, and may be used to provide an FFGLImage optimized to suit.
-    If you are passing the output of a renderer into another renderer as input for an image parameter, use FFGLRendererHintNone.
+    hint should indicate your intentions for the output, and may be used to provide an FFGLImage optimized to suit.
+    If you are passing the output of one FFGLRenderer into another FFGLRenderer as input for an image parameter, use FFGLRendererHintNone.
     size determines the dimensions of output frames. For CPU plugins, input frames must also match these dimensions.
 */
 - (id)initWithPlugin:(FFGLPlugin *)plugin context:(CGLContextObj)context pixelFormat:(NSString *)format outputHint:(FFGLRendererHint)hint size:(NSSize)size;
+
 - (FFGLPlugin *)plugin;
+
 - (CGLContextObj)context;
+
 - (NSString *)pixelFormat;
+
 - (NSSize)size;
+
 - (FFGLRendererHint)outputHint;
+
 /*
  - (BOOL)willUseParameterKey:(NSString *)key
     A plugin may ignore some of its image parameters under certain conditions. Use this method to discover if an input
     will be used with the parameters in their current state.
  */
 - (BOOL)willUseParameterKey:(NSString *)key;
-- (id)valueForParameterKey:(NSString *)key;
+
+/*
+ - (void)setValue:(id)value forParameterKey:(NSString *)key
+
+	Sets the value of the named parameter.
+	For boolean parameters, value should be a NSNumber with a boolean value.
+	For number parameters, value should be a NSNumber with a float value between 0.0 and 1.0.
+	For string parameters, value should be a NSString.
+	For image parameters, value should be a FFGLImage. For CPU-mode plugins, the image's dimensions must match
+	the renderer's dimensions.
+	key should be one of the keys obtained by a call to the FFGLRenderer's plugin's parameterKeys method.
+ */
 - (void)setValue:(id)value forParameterKey:(NSString *)key;
+
+- (id)valueForParameterKey:(NSString *)key;
+
 /*
  - (id)parameters
     Returns an object interested parties can bind to to get/set parameter values. Bind to aRenderer.parameters.aKey
  */
 - (id)parameters;
+
+/*
+ - (FFGLImage *)outputImage
+	Returns the product of the last call to renderAtTime: if such a call was made and succeeded, otherwise nil.
+ */
 - (FFGLImage *)outputImage;
+
+/*
+ - (BOOL)renderAtTime:(NSTimeInterval)time
+	Attempts to perform rendering using the currently set parameters at the specified time.
+	Returns YES if rendering succeeded, NO otherwise.
+	Rendering may fail if insufficient image parameters are set, if image parameters are set but they couldn't be used by
+	the renderer, or for other reasons.
+ */
 - (BOOL)renderAtTime:(NSTimeInterval)time;
 @end
