7c7db4fa820a347bf7b46d65e290c06687badda9
bangnoise <bangnoise@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2010-01-30 01:20:00 +0000

    Fix a test.

--- VVFFGL/UnitTests/FFGLPluginTests.m
+++ VVFFGL/UnitTests/FFGLPluginTests.m
@@ -20,13 +20,20 @@
 
 - (void)testReleasingPlugins
 {
+	// Because the FFGLPlugin class keeps track of instances so it can return a duplicate we check
+	// it is still releasing them when it should
+	
+	// Create an autorelease pool so we can force the release of our plugin
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	FFGLPlugin *plugin = [[[FFGLPluginManager sharedManager] plugins] lastObject];
 	NSString *path = [[[plugin attributes] objectForKey:FFGLPluginAttributePathKey] retain];
 	[[FFGLPluginManager sharedManager] unloadPlugin:plugin];
 	[pool drain];
+	// allocate something else so we are unlikely to get the same address in memory back
+	NSObject *padding = [[NSObject alloc] init];
 	FFGLPlugin *duplicate = [[[FFGLPlugin alloc] initWithPath:path] autorelease];
+	[padding release];
 	[path release];
-	STAssertTrue(plugin != duplicate, @"FFGLPlugin didn't release a plugin when it should have.");
+	STAssertTrue(plugin != duplicate, @"FFGLPlugin (probably) didn't release a plugin when it should have.");
 }
 @end
