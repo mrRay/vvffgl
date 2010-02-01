8a3fd2ba6ccd25b286ed486bc4ca6b5ba2480e15
bangnoise <bangnoise@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2010-02-01 21:43:17 +0000

    Removed redundant properties from FFGLImage (textureRectPixelsWide/High, bufferPixelsWide/High), move FFGLImageRep functions into their own file.

---
+++ VVFFGL/FrameworkSource/FFGLImageRep.h
@@ -0,0 +1,61 @@
+//
+//  FFGLImageRep.h
+//  VVFFGL
+//
+//  Created by Tom on 01/02/2010.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <OpenGL/OpenGL.h>
+#import "FFGLImage.h"
+#import "FFGLInternal.h"
+
+
+/*
+ FFGLImageRep
+ 
+	These functions are NOT thread-safe. They must be used in a thread-safe manner from FFGLImage.
+	No functions for introspection, access the struct members directly.
+ */
+typedef NSUInteger FFGLImageRepType;
+enum {
+    FFGLImageRepTypeTexture2D = 0,
+    FFGLImageRepTypeTextureRect = 1,
+    FFGLImageRepTypeBuffer = 2
+};
+
+// FFGLTextureInfo is in FFGLInternal.h as it's shared with plugins.
+
+typedef struct FFGLBufferInfo {
+    unsigned int	width;
+    unsigned int    height;
+    NSString		*pixelFormat;
+    const void		*buffer;
+} FFGLBufferInfo;
+
+typedef union FFGLImageRepCallback {
+    FFGLImageTextureReleaseCallback	textureCallback;
+    FFGLImageBufferReleaseCallback	bufferCallback;
+} FFGLImageRepCallback;
+
+typedef union FFGLImageRepInfo {
+    FFGLBufferInfo	bufferInfo;
+    FFGLTextureInfo	textureInfo;
+} FFGLImageRepInfo;
+
+typedef struct FFGLImageRep
+{
+    FFGLImageRepType		type;
+    BOOL					flipped;
+    FFGLImageRepInfo		repInfo;
+    FFGLImageRepCallback    releaseCallback;
+    void					*releaseContext;
+} FFGLImageRep;
+
+NSUInteger ffglBytesPerPixelForPixelFormat(NSString *format);
+FFGLImageRep *FFGLTextureRepCreateFromTexture(GLint texture, FFGLImageRepType type, NSUInteger imageWidth, NSUInteger imageHeight, NSUInteger textureWidth, NSUInteger textureHeight, BOOL isFlipped, FFGLImageTextureReleaseCallback callback, void *userInfo);
+FFGLImageRep *FFGLBufferRepCreateFromBuffer(const void *source, NSUInteger width, NSUInteger height, NSUInteger rowBytes, NSString *pixelFormat, BOOL isFlipped, FFGLImageBufferReleaseCallback callback, void *userInfo, BOOL forceCopy);
+FFGLImageRep *FFGLTextureRepCreateFromTextureRep(CGLContextObj cgl_ctx, const FFGLImageRep *fromTextureRep, FFGLImageRepType toTarget, BOOL useNPOT);
+FFGLImageRep *FFGLTextureRepCreateFromBufferRep(CGLContextObj cgl_ctx, const FFGLImageRep *fromBufferRep, FFGLImageRepType toTarget, BOOL useNPOT);
+FFGLImageRep *FFGLBufferRepCreateFromTextureRep(CGLContextObj cgl_ctx, const FFGLImageRep *fromTextureRep, NSString *pixelFormat);
+void FFGLImageRepDestroy(CGLContextObj cgl_ctx, FFGLImageRep *rep);
