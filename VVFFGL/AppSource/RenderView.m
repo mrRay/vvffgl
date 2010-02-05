5d5f01711c12431c73d7ec0af9fbf940da8527c1
vade <vade@4182d21d-85fa-2e6f-24a5-897ed14d5660>
2010-02-05 01:44:21 +0000

    Following our own advice on not setting GL_REPEAT on GL_TEXTURE_2D just to make sure things work ok. Its not needed. Switched to GL_CLAMP

--- VVFFGL/AppSource/RenderView.m
+++ VVFFGL/AppSource/RenderView.m
@@ -132,10 +132,11 @@
 	    glActiveTexture(GL_TEXTURE0);
 	    glEnable(GL_TEXTURE_2D);
 	    
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP);
 	    
 	    glBindTexture(GL_TEXTURE_2D, [image texture2DName]);
 	    
